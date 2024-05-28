01121b80a872699b742133532b4ee1666693a961
WalrusOnSeaIce <69725059+WalrusOnSeaIce@users.noreply.github.com>
2024-05-28 15:10:39 +0530

    Update dbops.py

--- dbops.py
+++ dbops.py
@@ -6,7 +6,6 @@ cur = db.cursor()
 
 def savetodb(record):
 	#table creation command: CREATE TABLE checksums (id INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, file VARCHAR(255), hash VARCHAR(12), original_checksum VARCHAR(60), file_checksum VARCHAR(60), result VARCHAR(30));
-	cur.execute("CREATE TABLE IF NOT EXISTS checksums (id INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, file VARCHAR(255), hash VARCHAR(12), original_checksum VARCHAR(60), file_checksum VARCHAR(60), result VARCHAR(30))")
 	date = datetime.now().strftime("%Y-%m-%d")
 	record.insert(0, date)
 	cur.execute("INSERT INTO checksums (date, file, hash, original_checksum, file_checksum, result) VALUES(?, ?, ?, ?, ?, ?)", (record[0], record[1], record[2], record[3], record[4], record[5]))
